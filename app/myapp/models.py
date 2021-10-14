c8a85d10a14d61e6342f41060049448d4ecec675
WindBlowDickCool <43900364+WindBlowDickCool@users.noreply.github.com>
2021-10-14 15:08:13 -0400

    updated architecture, added user model

--- app/myapp/models.py
+++ app/myapp/models.py
@@ -3,3 +3,8 @@ from django.db import models
 
 class Document(models.Model):
     docfile = models.FileField(upload_to='documents/%Y/%m/%d')
+
+class User(models.Model):
+    user = models.CharField(max_length=32)
+    email = models.EmailField(max_length=32)
+    pwd = models.CharField(max_length=32)
