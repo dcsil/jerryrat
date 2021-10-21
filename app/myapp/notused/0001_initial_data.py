46d116634e8c8d0b99f6d9faf96c5753744d59d1
Alchemist <44734206+JunmingZhang@users.noreply.github.com>
2021-10-21 19:08:57 -0400

    folder for code not used

---
+++ app/myapp/notused/0001_initial_data.py
@@ -0,0 +1,23 @@
+# http://flecox.github.io/django/2015/05/28/django-execute-sql-file-inside-a-migration-step.html
+import os
+
+from django.db import connection, migrations
+
+def load_data_from_sql(filename):
+    file_path = os.path.join(os.path.dirname(__file__), '../sql/', filename)
+    sql_statement = open(file_path).read()
+    with connection.cursor() as c:
+        c.execute(sql_statement)
+
+initial_data = lambda: load_data_from_sql('initDataBase.sql')
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('app_name', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.RunPython(initial_data),
+    ]
