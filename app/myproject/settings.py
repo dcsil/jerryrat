6aea513e16a0aedb90efd5cdd684ab289c82dd1d
Alchemist <44734206+JunmingZhang@users.noreply.github.com>
2021-10-27 06:28:57 -0400

    sql script debug

--- app/myproject/settings.py
+++ app/myproject/settings.py
@@ -82,6 +82,8 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 #         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
+DEFAULT_AUTO_FIELD='django.db.models.AutoField'
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
