e42fd861bfd0e21163b27c4f560392a4a9d5aee6
Alchemist <44734206+JunmingZhang@users.noreply.github.com>
2021-10-27 17:52:06 -0400

    add db table ORM model

--- app/myapp/models.py
+++ app/myapp/models.py
@@ -26,3 +26,38 @@ class PredictionModel(models.Model):
 
     def add_description(self, description):
         self.description = description
+
+
+# ORM for user data model
+# feed ML model data to learn
+# add to add a pyspark channel for feeding
+class Userdata(models.Model):
+    dataid = models.AutoField(primary_key=True)
+    age = models.IntegerField()
+    job = models.CharField(max_length=255)
+    marital = models.CharField(max_length=255)
+    education = models.CharField(max_length=255)
+    default = models.CharField(max_length=10)
+    housing = models.CharField(max_length=10)
+    loan = models.CharField(max_length=10)
+    contact = models.CharField(max_length=255)
+    month = models.CharField(max_length=5)
+    day_of_week = models.CharField(max_length=5)
+    duration = models.IntegerField()
+    campaign = models.IntegerField()
+    pdays = models.IntegerField()
+    previous = models.IntegerField()
+    poutcome = models.CharField(max_length=255)
+    emp_var_rate = models.DecimalField(db_column='emp.var.rate', max_digits=4,
+                                       decimal_places=1)  # Field renamed to remove unsuitable characters.
+    cons_price_idx = models.DecimalField(db_column='cons.price.idx', max_digits=5,
+                                         decimal_places=3)  # Field renamed to remove unsuitable characters.
+    cons_conf_idx = models.DecimalField(db_column='cons.conf.idx', max_digits=3,
+                                        decimal_places=1)  # Field renamed to remove unsuitable characters.
+    euribor3m = models.DecimalField(max_digits=4, decimal_places=3)
+    nr_employed = models.DecimalField(db_column='nr.employed', max_digits=5,
+                                      decimal_places=1)  # Field renamed to remove unsuitable characters.
+
+    class Meta:
+        managed = False
+        db_table = 'userdata'
