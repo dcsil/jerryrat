c2da161ec58ad23a51abac4c6c7a8ee4c6853fa8
Alchemist <44734206+JunmingZhang@users.noreply.github.com>
2021-10-28 02:25:44 -0400

    deploy on heroku

--- app/myproject/settings.py
+++ app/myproject/settings.py
@@ -11,6 +11,7 @@ import os
 import sentry_sdk
 from sentry_sdk.integrations.django import DjangoIntegration
 import dj_database_url
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -156,3 +157,6 @@ sentry_sdk.init(
 
 prod_db = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
