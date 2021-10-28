9b264c53e56c2e13d639e07f34612352e62b0b86
Alchemist <44734206+JunmingZhang@users.noreply.github.com>
2021-10-28 03:42:15 -0400

    deploy on heroku

--- app/myproject/settings.py
+++ app/myproject/settings.py
@@ -188,8 +188,8 @@ try:
 except Exception:
     print('Unexpected error:', sys.exc_info())
 
-prod_db = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(prod_db)
+# prod_db = dj_database_url.config(conn_max_age=500)
+# DATABASES['default'].update(prod_db)
 
 
 # Activate Django-Heroku.
