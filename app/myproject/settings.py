767a384661ada96455fe3f7a836b978cf6839af3
Alchemist <44734206+JunmingZhang@users.noreply.github.com>
2021-10-28 00:46:24 -0400

    deploy on heroku

--- app/myproject/settings.py
+++ app/myproject/settings.py
@@ -14,6 +14,7 @@ import os
 import sentry_sdk
 from sentry_sdk.integrations.django import DjangoIntegration
 import dj_database_url
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
