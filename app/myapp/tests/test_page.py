358b46ac7df45ceca7f350aedcd6b1f4449fa71c
Alchemist <44734206+JunmingZhang@users.noreply.github.com>
2021-11-02 02:34:45 -0400

    test case for file uploader

--- app/myapp/tests/test_page.py
+++ app/myapp/tests/test_page.py
@@ -2,11 +2,12 @@ from django.test import TestCase, Client
 from django.views import *
 from myapp.models import *
 from myapp import urls
+from myapp.utils.tableUploader import uploadFileToDB
 from django.urls import reverse, resolve
 # Create your tests here.
 
 
-class TestPageIntegrity(TestCase, Client):
+class TestIntegrity(TestCase, Client):
     page_names = urls.page_names
 
     def test_page(self):
@@ -15,3 +16,13 @@ class TestPageIntegrity(TestCase, Client):
             print("Testing " + i)
             response = client.get(reverse(i))
             self.assertEquals(response.status_code, 200)
+
+    def test_fileupload(self):
+        raised = False
+        try:
+            uploadFileToDB("app/static/dataset/testdatabase.csv")
+            uploadFileToDB("app/static/dataset/testdatabase.xlsx")
+        except:
+            raised = True
+        finally:
+            self.assertEqual(raised, False)
