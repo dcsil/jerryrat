7fcbf9cd75b3c86e8cd2652e2e25577ebfc4e826
Alchemist <44734206+JunmingZhang@users.noreply.github.com>
2021-11-03 20:08:08 -0400

    complete training partially

--- app/myapp/pred/train.py
+++ app/myapp/pred/train.py
@@ -1,11 +1,23 @@
 import xgboost as xgb
 from sklearn.model_selection import train_test_split
 import json
+import os
 
 from load_model import load_model
+from load_config import load_config
 
 def train(model_path="models/model_init.json", config_path="configs/config_init.json"):
-    model = load_model(model_path)
+    model = None
+    params = None
+    if os.path.exists(model_path):
+        model = load_model(model_path)
+    if os.path.exists:
+        params = load_config(config_path)
+    else:
+        params = {
+
+        }
+
 
 if __name__ == "__main__":
     pass
