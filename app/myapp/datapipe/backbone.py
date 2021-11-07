a8a19533930d6de5d4877777dd852fe16c650d93
Alchemist <44734206+JunmingZhang@users.noreply.github.com>
2021-11-07 05:00:04 -0500

    add ToDo for backbone

--- app/myapp/datapipe/backbone.py
+++ app/myapp/datapipe/backbone.py
@@ -2,6 +2,7 @@ import pyspark
 
 def createBackBone():
     sc = pyspark.SparkContext()
+    #TODO: practice with spark, create a pandas.dataframe from database data periodically (around 10s)
 
 if __name__ == "__main__":
     createBackBone()
