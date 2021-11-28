956795fd8b45cc591a88e1962af5c613d168dbec
WindBlowDickCool <43900364+WindBlowDickCool@users.noreply.github.com>
2021-11-27 23:39:01 -0500

    Implemented model configuration page

--- app/myapp/datapipe/customize_config.py
+++ app/myapp/datapipe/customize_config.py
@@ -2,18 +2,19 @@ import json
 
 
 # the input should be a dictionary with configurations
-def customize_config(customized_configs=''):
+def customize_config(customized_configs, user):
     # reads and clear file
+    path = './users/' + user + '/config.json'
     try:
-        with open("./config/config.json", "r") as fp:
+        with open(path, "r") as fp:
             configs = json.load(fp)
             for settings in configs:
-                configs[settings] = customized_configs[settings]
-
-        with open("./config/config.json", "w+") as fp:
+                if settings in customized_configs:
+                    if customized_configs[settings] != "":
+                        configs[settings] = customized_configs[settings]
+        with open(path, "w+") as fp:
             json.dump(configs, fp)
     except KeyError:
         print('Incorrect configuration data! Please check if the configurations are updated...')
 
 
-# customize_config({'numFetchRows': 200, 'period': 30})
