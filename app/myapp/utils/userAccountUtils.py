5eee4dc07389657c7ebf0ff6f8cccba2e5d0c312
Angelo Huang <chermissote@gmail.com>
2021-12-06 01:35:30 -0500

    Removed some email methods

--- app/myapp/utils/userAccountUtils.py
+++ app/myapp/utils/userAccountUtils.py
@@ -11,21 +11,3 @@ def send_mail(to, template, context):
     msg = EmailMultiAlternatives(context['subject'], text_content, settings.DEFAULT_FROM_EMAIL, [to])
     msg.attach_alternative(html_content, 'text/html')
     msg.send()
-
-def send_reset_password_email(request, email, token, uid):
-    context = {
-        'subject': gettext_lazy('Retrieve your password'),
-        'uri': request.build_absolute_uri(
-            reverse('retrieve_password_confirm', kwargs={'uidb64': uid, 'token': token})),
-    }
-
-    send_mail(email, 'retrieve_password_email', context)
-
-
-def send_forgotten_username_email(email, username):
-    context = {
-        'subject': gettext_lazy('Your username'),
-        'username': username,
-    }
-
-    send_mail(email, 'forgotten_username', context)
