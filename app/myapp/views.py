a11338fe1f3c298847f1f0ae23dab7777937b74c
Angelo Huang <chermissote@gmail.com>
2021-12-06 02:27:16 -0500

    Removed changePassword method

--- app/myapp/views.py
+++ app/myapp/views.py
@@ -228,18 +228,4 @@ class LogOut(LoginRequiredMixin, BaseLogoutView):
     template_name = 'logout.html'
 
 
-class ChangePassword(BasePasswordChangeView):
-    template_name = 'change_password.html'
-
-    def form_valid(self, form):
-        # Change the password
-        user = form.save()
-
-        # Re-authentication
-        login(self.request, user)
-
-        messages.success(self.request, _('Your password was changed.'))
-        return redirect('change_password')
-
-
 # ==============================================================================================================
